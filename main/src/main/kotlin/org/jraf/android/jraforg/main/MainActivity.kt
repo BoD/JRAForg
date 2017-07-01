369f6128dd4109434504eaf132648d6f6c740b41
BoD <BoD@JRAF.org>
2017-07-01 14:35:17 +0200

    Initial commit

---
+++ main/src/main/kotlin/org/jraf/android/jraforg/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.jraf.android.jraforg.main
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+    }
+}
